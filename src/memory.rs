7a87e538ea60f18ac2c67dc92df057654f6bf943
David <davidjovanovski62@gmail.com>
2024-05-19 10:55:31 +0200

    chore(memory): add new fn for initializing memory

--- src/memory.rs
+++ src/memory.rs
@@ -5,3 +5,12 @@ pub struct Memory {
     pub operations: Vec<Operation>,
     pub values: Vec<i32>,
 }
+
+impl Memory {
+    pub fn new(operations: Vec<Operation>) -> Memory {
+        Memory {
+            operations,
+            values: vec![],
+        }
+    }
+}
